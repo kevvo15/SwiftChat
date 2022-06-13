5327742a214fbb10b87a1cbfb6e71cde9a79936a
Kevin Sandoval <kevinsandoval.co@gmail.com>
2022-06-13 13:56:16 -0600

    Initial Commit

---
+++ SwiftChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftChat
+//
+//  Created by Kevin Sandoval on 6/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftChat/SwiftChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatApp.swift
+//  SwiftChat
+//
+//  Created by Kevin Sandoval on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
