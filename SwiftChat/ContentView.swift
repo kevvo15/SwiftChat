f5449a9b542b831b10c1afe0d73392ea186e066a
Kevin Sandoval <kevinsandoval.co@gmail.com>
2022-06-13 14:29:38 -0600

    created landing login page for application

--- SwiftChat/ContentView.swift
+++ SwiftChat/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            ScrollView {
+                
+                VStack {
+                    Picker(selection: $isLoginMode, label:
+                            Text("Picker Here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "person.fill")
+                            .font(.system(size:64))
+                            .padding()
+                    }
+                    
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                    SecureField("Password", text: $password)
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                }
+                .padding()
+                
+                
+            }
+            .navigationTitle("Create Account")
+        }
     }
 }
 
