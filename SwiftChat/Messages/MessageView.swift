807c622fcb77d4bbc906eebc38e1d9d4306eae67
Kevin Sandoval <kevinsandoval.co@gmail.com>
2022-06-14 19:03:02 -0600

    updated messages view interface; added new message button

--- SwiftChat/Messages/MessageView.swift
+++ SwiftChat/Messages/MessageView.swift
@@ -8,66 +8,112 @@
 import SwiftUI
 
 struct MessageView: View {
+    
+    @State private var showLogoutOptions = false
+    
+    private var customNavBar: some View {
+        HStack(spacing: 16) {
+            Image(systemName: "person.fill")
+                .font(.system(size: 34, weight: .heavy))
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("USERNAME")
+                    .font(.system(size: 24, weight: .bold))
+                HStack {
+                    Circle()
+                        .foregroundColor(.green)
+                        .frame(width: 12, height: 12)
+                    Text("Online")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color(.lightGray))
+                }
+            }
+            Spacer()
+            Button {
+                showLogoutOptions.toggle()
+            } label: {
+                Image(systemName: "gear")
+                    .foregroundColor(Color(.label))
+                    .font(.system(size: 24, weight: .bold))
+            }
+        }
+        .padding()
+        .actionSheet(isPresented: $showLogoutOptions) {
+            .init(title: Text("Settings"), message: Text("What would you like to do?"), buttons: [
+                .destructive(Text("Sign Out"), action: {
+                    print("Handle Sign Out")
+                }),
+                .cancel()
+            ])
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            
             VStack {
-                HStack(spacing: 16) {
-                    Image(systemName: "person.fill")
-                        .font(.system(size: 34, weight: .heavy))
-                    
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("USERNAME")
-                            .font(.system(size: 24, weight: .bold))
-                        HStack {
-                            Circle()
-                                .foregroundColor(.green)
-                                .frame(width: 12, height: 12)
-                            Text("Online")
+                customNavBar
+                messagesView
+            }
+            .overlay(newMessageButton, alignment: .bottom)
+            .navigationBarHidden(true)
+        }
+    }
+    
+    private var messagesView: some View {
+        ScrollView {
+            ForEach(0..<10, id: \.self) { num in
+                VStack {
+                    HStack(spacing: 16) {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 32))
+                            .padding(8)
+                            .overlay(RoundedRectangle(cornerRadius: 44)
+                                .stroke(Color(.label), lineWidth: 1.5)
+                            )
+                        VStack(alignment: .leading) {
+                            Text("Username")
+                                .font(.system(size: 16, weight: .bold))
+                            Text("Message sent to user")
                                 .font(.system(size: 14))
                                 .foregroundColor(Color(.lightGray))
                         }
+                        Spacer()
+                        
+                        Text("22d")
+                            .font(.system(size: 14, weight: .semibold))
                     }
-                    Spacer()
-                    Image(systemName: "gear")
-                }.padding()
-                
-                ScrollView {
-                    ForEach(0..<10, id: \.self) { num in
-                        VStack {
-                            HStack(spacing: 16) {
-                                Image(systemName: "person.fill")
-                                    .font(.system(size: 32))
-                                    .padding(8)
-                                    .overlay(RoundedRectangle(cornerRadius: 44)
-                                        .stroke(Color(.black), lineWidth: 1.5)
-                                    )
-                                VStack(alignment: .leading) {
-                                    Text("Username")
-                                        .font(.system(size: 16, weight: .bold))
-                                    Text("Message sent to user")
-                                        .font(.system(size: 14))
-                                        .foregroundColor(Color(.lightGray))
-                                }
-                                Spacer()
-                                
-                                Text("22d")
-                                    .font(.system(size: 14, weight: .semibold))
-                            }
-                            Divider()
-                                .padding(.vertical, 8)
-                        }.padding(.horizontal)
-                    }
-                }
+                    Divider()
+                        .padding(.vertical, 8)
+                }.padding(.horizontal)
+            }.padding(.bottom, 50)
+        }
+    }
+    
+    private var newMessageButton: some View {
+        Button {
+            
+        } label: {
+            HStack {
+                Spacer()
+                Text("+ New Message")
+                    .font(.system(size: 16, weight: .bold))
+                Spacer()
             }
-            .navigationBarHidden(true)
-//            .navigationTitle("Chat Messages")
+            .foregroundColor(.white)
+            .padding(.vertical)
+            .background(Color.blue)
+            .cornerRadius(32)
+            .padding(.horizontal)
+            .shadow(radius: 10)
         }
     }
 }
 
 struct MessageView_Previews: PreviewProvider {
     static var previews: some View {
+        MessageView()
+            .preferredColorScheme(.dark)
+        
         MessageView()
     }
 }
