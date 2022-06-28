bc789b0858007ca635f68da4374bdcdcd01d064c
Kevin Sandoval <kevinsandoval.co@gmail.com>
2022-06-28 15:52:23 -0600

    feat: refactored firebase manager file into utils folder

---
+++ SwiftChat/Utils/FirebaseManager.swift
@@ -0,0 +1,29 @@
+//
+//  FirebaseManager.swift
+//  SwiftChat
+//
+//  Created by Kevin Sandoval on 6/28/22.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    let storage: Storage
+    let firestore: Firestore
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        self.storage = Storage.storage()
+        self.firestore = Firestore.firestore()
+        
+        super.init()
+    }
+    
+}
