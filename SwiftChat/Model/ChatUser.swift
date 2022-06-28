89e2cae607c32928ab3b819238b2350e77c6dc6a
Kevin Sandoval <kevinsandoval.co@gmail.com>
2022-06-28 17:09:22 -0600

    feat: created Chat User model for messaging

---
+++ SwiftChat/Model/ChatUser.swift
@@ -0,0 +1,18 @@
+//
+//  ChatUser.swift
+//  SwiftChat
+//
+//  Created by Kevin Sandoval on 6/28/22.
+//
+
+import Foundation
+
+struct ChatUser {
+    let uid, email, profileImageUrl: String
+    
+    init(data: [String: Any]) {
+        self.uid = data["uid"] as? String ?? ""
+        self.email = data["email"] as? String ?? ""
+        self.profileImageUrl = data["profileImageUrl"] as? String ?? ""
+    }
+}
