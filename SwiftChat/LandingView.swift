a0a7b23bf13294f06b354cdcbcf6fefbe9372ef8
Kevin Sandoval <kevinsandoval.co@gmail.com>
2022-06-28 15:53:36 -0600

    feat: created shared firebase manager for logged in users

--- SwiftChat/LandingView.swift
+++ SwiftChat/LandingView.swift
@@ -8,26 +8,6 @@
 import SwiftUI
 import Firebase
 
-class FirebaseManager: NSObject {
-    
-    let auth: Auth
-    let storage: Storage
-    let firestore: Firestore
-    
-    static let shared = FirebaseManager()
-    
-    override init() {
-        FirebaseApp.configure()
-        
-        self.auth = Auth.auth()
-        self.storage = Storage.storage()
-        self.firestore = Firestore.firestore()
-        
-        super.init()
-    }
-    
-}
-
 struct LandingView: View {
     
     @State private var isLoginMode = false
